1b87511ff6121b21a608c24ad26528e13ae24490
Fc500 <66227413+Fc500@users.noreply.github.com>
2021-03-01 10:04:21 -0500

    Update addons.js

--- JS/addons.js
+++ JS/addons.js
@@ -1,4 +1,4 @@
-function myFunction() {
+function myFunction2() {
     var input, filter, ul, li, a, i, txtValue;
     input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
@@ -13,4 +13,23 @@ function myFunction() {
             li[i].style.display = "none";
         }
     }
+}
+
+function myFunction() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
 }
