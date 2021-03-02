6284e5141e7b55bd88d97f96253edd50d8652120
Fc500 <66227413+Fc500@users.noreply.github.com>
2021-03-02 12:38:58 -0500

    Update results.js

--- JS/results.js
+++ JS/results.js
@@ -3,3 +3,8 @@ var customTitle = sessionStorage.getItem("title");
 function setTitle() {
   document.getElementById("head").innerHTML = customTitle;
 }
+
+var x = document.getElementById("get").innerHTML;
+function myFunction() {
+  document.getElementById("testy").innerHTML = x;
+}
