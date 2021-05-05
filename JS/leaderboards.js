22b9d1b346dfc492ceba846d82d368c95f7297e6
Fc500 <66227413+Fc500@users.noreply.github.com>
2021-05-05 16:18:12 -0400

    Update leaderboards.js

--- JS/leaderboards.js
+++ JS/leaderboards.js
@@ -150,3 +150,18 @@ function resetUserInfo() {
   location.reload();
 }
 
+function toggleTheme() {
+            // Obtains an array of all <link>
+            // elements.
+            // Select your element using indexing.
+            var theme = document.getElementsByTagName('link')[0];
+  
+            // Change the value of href attribute 
+            // to change the css sheet.
+            if (theme.getAttribute('href') == '../CSS/leaderboard.css') {
+                theme.setAttribute('href', '../CSS/leaderboardDarkMode.css');
+            } else {
+                theme.setAttribute('href', '../CSS/leaderboard.css');
+            }
+        }
+
