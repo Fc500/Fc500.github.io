b861041dece185b05e2410345ca4c75c21e46cd5
Fc500 <66227413+Fc500@users.noreply.github.com>
2021-05-04 21:31:43 -0400

    Update game.js

--- JS/game.js
+++ JS/game.js
@@ -1,22 +1,23 @@
 var character = document.getElementById("character");
-document.addEventListener("click",jump);
-function jump(){
-    if(character.classList == "animate"){return;}
-    character.classList.add("animate");
-    setTimeout(removeJump,300); //300ms = length of animation
-}
-
-function removeJump(){
-    character.classList.remove("animate");
-}
-
 var block = document.getElementById("block");
-function checkDead(){
+var counter=0;
+function jump(){
+    if(character.classList == "animate"){return}
+    character.classList.add("animate");
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },300);
+}
+var checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
-        alert("Game over");
+        block.style.animation = "none";
+        alert("Game Over. score: "+Math.floor(counter/100));
+        counter=0;
+        block.style.animation = "block 1s infinite linear";
+    }else{
+        counter++;
+        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
     }
-}
-
-setInterval(checkDead, 10);
+}, 10);
