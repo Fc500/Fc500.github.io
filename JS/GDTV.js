59dc04a876cd3d4387b30b2cd05a79df6f38f707
Fc500 <66227413+Fc500@users.noreply.github.com>
2021-08-22 16:53:57 -0400

    Create GDTV.js

---
+++ JS/GDTV.js
@@ -0,0 +1,46 @@
+var video_container = document.getElementsByClassName('sections' );
+var add_videos;
+
+// Full Array example
+// const videos = [[["video1", "video2"], ["video3", "video4"]], [["video3, video 4"]]] 
+
+// Main Video Array
+
+const videos = new Array ();
+
+// Video Section 1
+
+videos[0] = new Array ();
+
+// Video Subsection 1 
+videos[0][0] = new Array ("https://www.youtube-nocookie.com/embed/35CD99iRgZM", "https://www.youtube-nocookie.com/embed/35CD99iRgZM");
+
+// Video Subsection 2
+videos[0][1] = new Array ("https://www.youtube-nocookie.com/embed/yOAGawn3XO4", "https://www.youtube-nocookie.com/embed/yOAGawn3XO4");
+
+// Video Section 2
+videos[1] = new Array ();
+
+// Video Section 2 Subesection
+videos[1][0] = new Array ("https://www.youtube-nocookie.com/embed/sEs3SBbNgiU", "https://www.youtube-nocookie.com/embed/sEs3SBbNgiU");
+ 
+var remove_function = document.getElementsByClassName("generator");
+
+function generateVideos(array, arrsection, section, generator) {
+
+var arrayLength = videos[array][arrsection].length;
+
+for (var i = 0; i < arrayLength; i++) {
+    add_videos = document.createElement('iframe');
+    add_videos.height = 300
+    add_videos.width = 250
+    add_videos.src = videos[array][arrsection][i];
+    
+    //Do something
+
+    video_container[section].appendChild(add_videos);
+        }
+remove_function[generator].onclick = function() {
+	window.alert("Section " + generator + " has been generated.");
+	}
+}
