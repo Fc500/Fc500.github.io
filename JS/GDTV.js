53ee3ac492754de633f952ad09e0bf7c84aa7db6
Fc500 <66227413+Fc500@users.noreply.github.com>
2021-08-24 15:27:36 -0400

    Update GDTV.js

--- JS/GDTV.js
+++ JS/GDTV.js
@@ -1,6 +1,6 @@
 // Update
 window.onload = function starting() {
-	window.alert("version 0.35 is working!");
+	window.alert("version 0.355 is working!");
 	generateVideos(0, 0, 0, 0);
 	generateVideos(0, 1, 1, 1); 
 	generateVideos(1, 0, 2, 2);
