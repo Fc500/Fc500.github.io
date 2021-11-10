aa727f202d40ecd02ef97dba0961d85ea65b773a
Fc500 <66227413+Fc500@users.noreply.github.com>
2021-11-10 09:03:50 -0500

    Create bottom_navbar.js

---
+++ climate/bottom_navbar.js
@@ -0,0 +1,67 @@
+let elementsInNav = ['home', 'news', 'contact'];
+
+let homeNav = document.getElementById('home');
+let newsNav = document.getElementById('news');
+let contactNav = document.getElementById('contact');
+let aboutNav = document.getElementById('about');
+
+
+
+/* for (i = 0; i < elementsInNav.length; i++;) {
+  if (elementsInNav[i] == element) {
+
+    let elementToBeRemoved = element;
+    elementsInNav.splice(i, i + 1);
+
+
+  }
+} 
+*/
+function openElement(element) {
+  var elementRaw = element;
+  window.alert(elementRaw);
+    if (elementRaw == 'home') {
+      if (homeNav.style.display == 'none') { 
+        homeNav.style.display = 'block';
+        newsNav.style.display = 'none';
+        contactNav.style.display = 'none';
+        aboutNav.style.display = 'none';
+
+      } else {
+        homeNav.style.display = 'none';
+      }
+    } else if (elementRaw == 'news') {
+      if (newsNav.style.display == 'none') { 
+        newsNav.style.display = 'block';
+        homeNav.style.display = 'none';
+        contactNav.style.display = 'none';
+        aboutNav.style.display = 'none';
+
+      } else {
+        newsNav.style.display = 'none';
+      }
+    } else if (elementRaw == 'contact') {
+      if (contactNav.style.display == 'none') { 
+        contactNav.style.display = 'block';
+        newsNav.style.display = 'none';
+        homeNav.style.display = 'none';
+        aboutNav.style.display = 'none';
+
+      } else {
+        contactNav.style.display = 'none';
+      }    
+    } else if (elementRaw == 'about') {
+      if (aboutNav.style.display == 'none') { 
+        aboutNav.style.display = 'block';
+        newsNav.style.display = 'none';
+        homeNav.style.display = 'none';
+        contactNav.style.display = 'none';
+
+      } else {
+        aboutNav.style.display = 'none';
+      }
+
+    }
+}
+ 
+// to add a class name use the variable.className += "";
