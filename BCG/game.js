afbacae4aea695724a4da2000b77e9b8796856dc
Fc500 <66227413+Fc500@users.noreply.github.com>
2022-09-01 07:46:40 -0400

    updated navbar

--- BCG/game.js
+++ BCG/game.js
@@ -12,3 +12,12 @@ class trumpet {
     }
 
 }
+
+function myFunction() {
+    var x = document.getElementById("myNavbar");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+  }
