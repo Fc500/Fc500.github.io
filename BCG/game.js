e01f7f4e3227b77458244ffbc8eefbe0e41c69dd
Fc500 <66227413+Fc500@users.noreply.github.com>
2022-09-01 11:26:29 -0400

    trying to add player cards

--- BCG/game.js
+++ BCG/game.js
@@ -1,7 +1,34 @@
 // Player
 
 window.onload = function loading() {
-  window.alert("v0.05");
+  window.alert("v0.07");
+}
+
+// Variables
+
+let scoutResults = document.getElementById("scoutResults");
+
+let playerTitle = "";
+let playerCardInfo = `
+<div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="img_avatar.png" alt="Avatar" style="width:300px;height:300px;">
+    </div>
+    <div class="flip-card-back">
+      <h1>${playerTitle}</h1> 
+      <p>Architect & Engineer</p> 
+      <p>We love that guy</p>
+    </div>
+  </div>
+</div>`;
+
+function scoutPlayers() {
+  const createPlayerCard = document.createElement("div");
+  playerTitle = "Test1";
+  createPlayerCard.innerHTML = playerCardInfo;
+  scoutPlayers.appendChild(createPlayerCard);
+  window.alert("Test Complete");
 }
 
 
