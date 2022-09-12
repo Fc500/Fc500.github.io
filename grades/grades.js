aafc0ca656645d3043452a98c7416fb0c7d012df
Fc500 <66227413+Fc500@users.noreply.github.com>
2022-09-12 13:06:29 -0400

    test4

--- grades/grades.js
+++ grades/grades.js
@@ -4,12 +4,12 @@ let gpaDisp = document.getElementById("gpa");
 let gpa;
 
 window.onload = function loading() {
-    window.alert("0.02");
+    window.alert("0.03");
 }
 
 function checkGrades() {
     window.alert("In checkGrades function")
-    let value = calculateValues.options[calculateValues.index].text;
+    let value = calculateValues.options[calculateValues.selectedIndex].text;
     window.alert(value);
     if (value == "A") {
         gpa = 4;
