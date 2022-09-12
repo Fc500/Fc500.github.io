b17d7bf65d69d58cb1fdb85193efc2010a0e33cd
Fc500 <66227413+Fc500@users.noreply.github.com>
2022-09-12 12:44:44 -0400

    testing

---
+++ grades/grades.js
@@ -0,0 +1,19 @@
+let calculateButton = document.getElementById("calculateButton");
+let calculateValues = document.getElementById("gradeslabel");
+let gpaDisp = document.getElementById("gpa");
+let gpa;
+
+window.onload = function loading () {
+    window.alert("0.01");
+}
+
+function checkGrades() {
+    let value = calculateValues.value();
+    window.alert(value);
+    if (value == "A") {
+        gpa = 4;
+    }
+
+    gpaDisp.innerHTML = gpa;
+
+}
