785f052093177714daed6f94aad9b94d9160633e
Fc500 <66227413+Fc500@users.noreply.github.com>
2022-10-03 22:04:28 -0400

    revamped everything
    changed how it looked

--- BCG/game.js
+++ BCG/game.js
@@ -112,27 +112,3 @@ function scoutPlayers(amount) {
   }
 }
 
-
-function navbarFunction() {
-    var x = document.getElementById("myNavbar");
-    if (x.className === "navbar") {
-      x.className += " responsive";
-    } else {
-      x.className = "navbar";
-    }
-  }
-
-  function switchFunction(chosenSection) {
-    let sections = document.getElementsByClassName("section");
-    let sidenavSections = document.getElementsByClassName("navChild");
-
-    for (let i = 0; i < sections.length; i++) {
-        sections[i].style.display = "none";
-        sidenavSections[i].style.fontWeight = "normal";   
-        sidenavSections[i].classList.remove("active");
-   
-    } 
-    sidenavSections[chosenSection].classList.add("active");
-    sections[chosenSection].style.display = "block";
-    sidenavSections[chosenSection].style.fontWeight = "bold";
-}
