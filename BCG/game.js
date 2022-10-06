3292c8168394b806e639a0a269de5724b4b77f0d
Fc500 <66227413+Fc500@users.noreply.github.com>
2022-10-06 11:45:48 -0400

    skdjfg

--- BCG/game.js
+++ BCG/game.js
@@ -19,7 +19,7 @@ let playerBand = [];
 let playerBandIndex = 0;
 
 window.onload = function loading() {
-  window.alert("v0.23.5s");
+  window.alert("v0.23.6s");
   moneyDisp.innerHTML = money;
   influenceDisp.innerHTML = influence;
 
