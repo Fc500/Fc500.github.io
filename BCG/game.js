ec09364ca8626389286c7950171772d47ca818fb
Fc500 <66227413+Fc500@users.noreply.github.com>
2022-10-07 07:26:32 -0400

    last css

--- BCG/game.js
+++ BCG/game.js
@@ -19,7 +19,7 @@ let playerBand = [];
 let playerBandIndex = 0;
 
 window.onload = function loading() {
-  window.alert("v0.25");
+  window.alert("v0.26");
   moneyDisp.innerHTML = money;
   influenceDisp.innerHTML = influence;
 
