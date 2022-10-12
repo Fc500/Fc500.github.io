2bb8c14d841c80a6519e2d96868e410bd9a15e99
Fc500 <66227413+Fc500@users.noreply.github.com>
2022-10-11 23:00:15 -0400

    cscscs
    a lot of cool stuff

--- BCG/game.js
+++ BCG/game.js
@@ -212,18 +212,65 @@ function numFormatter(num) {
 var close = document.getElementsByClassName("alert_closebtn");
 
 function makeAlert(message, type) {
-	let add = document.getElementById("alerts");
-    add.innerHTML += `<div class="alert ${type}">
-  <span class="alert_closebtn">&times;</span>  
+	const add = document.getElementById("alerts");
+  let id = generateRandom(1, 100000);
+  const messageDisp = `<div class="alert ${type}" id="${id}">
+  <span class="alert_closebtn" onclick="closeAlert(${id})">&times;</span>  
   <strong>${message}</strong>
 </div>`;
 
+add.innerHTML += messageDisp;
+
+console.log("type of DIV: " + typeof id);
+
+setTimeout(function() {
+  let alert = document.getElementById(id);
+  alert.style.opacity = "0";
+  setTimeout(function() {
+    alert.style.display = "none";
+    alert.remove();
+  }, 600);
   
-for (let i = 0; i < close.length; i++) {
-  close[i].onclick = function(){
-    var div = this.parentElement;
-    div.style.opacity = "0";
-    setTimeout(function(){ div.style.display = "none"; }, 600);
-  }
+}, 7500);
+
 }
+
+function closeAlert(id) {
+  let alert = document.getElementById(id);
+  alert.style.opacity = "0";
+  setTimeout(function(){ 
+    alert.style.display = "none"; 
+    alert.remove();
+    console.log(id);
+  }, 600);
+}
+
+
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
 }
