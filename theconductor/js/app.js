6f27b460c6cbeb80054609081979404509d0f91e
Fc500 <66227413+Fc500@users.noreply.github.com>
2022-10-18 09:18:14 -0400

    css changes

--- theconductor/js/app.js
+++ theconductor/js/app.js
@@ -16,7 +16,7 @@ var firstNames = ["James", "Mary", "Robert", "Patricia", "Michael", "Linda", "Da
 var lastNames = ["Smith", "Johnson", "Williams", "Brown", "Jones", "Garcia", "Miller", "Davis", "Gardner", "Trench"];
 
 window.onload = function loading() {
-    window.alert("v0.40");
+    window.alert("v0.41");
     moneyDisp.innerHTML = numFormatter(money);
     influenceDisp.innerHTML = influence;
 
