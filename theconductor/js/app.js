7c71d253eadf89549ae4a2b4b277e11085a2d2b8
Fc500 <66227413+Fc500@users.noreply.github.com>
2022-10-18 13:10:18 -0400

    its all comin' together

--- theconductor/js/app.js
+++ theconductor/js/app.js
@@ -16,7 +16,7 @@ var firstNames = ["James", "Mary", "Robert", "Patricia", "Michael", "Linda", "Da
 var lastNames = ["Smith", "Johnson", "Williams", "Brown", "Jones", "Garcia", "Miller", "Davis", "Gardner", "Trench"];
 
 window.onload = function loading() {
-    window.alert("v0.45");
+    window.alert("v0.46");
     moneyDisp.innerHTML = numFormatter(money);
     influenceDisp.innerHTML = influence;
 
