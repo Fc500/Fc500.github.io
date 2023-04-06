9ff72dfb39b6e336597652525ad67b70058eac5d
Fc500 <66227413+Fc500@users.noreply.github.com>
2023-04-06 10:12:09 -0400

    Update jamman.js

--- jam/jamman.js
+++ jam/jamman.js
@@ -675,11 +675,4 @@ let checkPlantBreedTimer = setInterval(checkPlantsBreed, 5000);
 let checkMoneyTimer = setInterval(checkMoney, 1000);
 let notifChecker = setInterval(updateNotif, 10000);
 
-if ("serviceworker" in navigator) {
-  window.addEventListener("load", function() {
-    navigator.serviceWorker
-      .register("/serviceWorker.js")
-      .then(res => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err))
-  })
-}
+
